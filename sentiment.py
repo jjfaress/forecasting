c8236decb47352006485ffbf6b38bdade32d99bf
jjfaress <jjfaress@gmail.com>
2022-03-03 16:42:48 -0500

    trying other bert sizes

--- sentiment.py
+++ sentiment.py
@@ -86,6 +86,7 @@ def clean(text):
     # nopun = ''.join(letter for letter in lower if letter not in pun)
     rere = re.sub('@[^\s]+', '', txt)
     rere = re.sub('$[^\s]+', '', rere)
+    rere = re.sub('https?:\/\/t.co/[\w]+', '', rere)
     # noascii = rere.encode("ascii", "ignore").decode()
     # nostop = ' '.join([word for word in noascii.split(' ') if word not in stop])
     # result = ' '.join(
@@ -100,6 +101,12 @@ def schedule(epoch, lr):
         return lr * 0.1
     return lr
 
+#bert L12: https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4
+#bert L12 preprocess: https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3
+# bert small preprocess: https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3
+# bert small: https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-128_A-2/2
+#bert L24: https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/4
+#bert L24 preprocess: https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3
 
 def build():
     text_input = layers.Input(shape=(), dtype=tf.string, name='text')
@@ -113,8 +120,15 @@ def build():
         name='bert')
     outputs = encoder(encoder_inputs)
     net = outputs['pooled_output']
-    net = layers.Dropout(0.1)(net)
+    net = layers.Dropout(0.2)(net)
+    # net = layers.Reshape((1, 128))(net)
+    # cnn = layers.Conv1D(128, 1, input_shape=(1, 128))(net)
+    # cnn = layers.MaxPooling1D(padding='same')(net)
+    # # lstm = Bidirectional(layers.LSTM(32, return_sequences=True))(cnn)
+    # lstm = Bidirectional(layers.LSTM(64))(net)
+    net = layers.Dense(16)(net)
     net = layers.Dense(3, activation='softmax', name='classifier')(net)
+
     return tf.keras.Model(text_input, net)
 
 
@@ -147,7 +161,7 @@ headlines.columns = ['score', 'text']
 
 data = financecsv.append(headlines, ignore_index=True)
 data = data.append(dyna, ignore_index=True)
-data = data.append(stanfordcsv[:120000], ignore_index=True)
+data = data.append(stanfordcsv[:125000], ignore_index=True)
 
 data['text'] = data['text'].apply(lambda r: clean(r))
 data['score'] = data['score'].apply(scoreConfig)
@@ -156,14 +170,14 @@ data.dropna(inplace=True)
 data.drop_duplicates(inplace=True)
 data = data.sample(frac=1)
 
-sns.set()
-sns.displot(data['score'])
-plt.show()
+# sns.set()
+# sns.displot(data['score'])
+# plt.show()
 
 train, test = train_test_split(data, test_size=0.2)
 train, val = train_test_split(train, test_size=0.2)
 
-BATCH_SIZE = 256
+BATCH_SIZE = 16
 
 trainraw = (
     tf.data.Dataset.from_tensor_slices(
@@ -198,7 +212,7 @@ val = valraw.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 model = build()
 model.summary()
 stepsEpoch = tf.data.experimental.cardinality(train).numpy()
-epochs = 5
+epochs = 10
 trainSteps = stepsEpoch * epochs
 warmup = int(0.1 * trainSteps)
 init_lr = 3e-5
@@ -220,88 +234,3 @@ history = model.fit(
     validation_data=val,
     epochs=epochs
 )
-
-# preprocess = hub.load('https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3')
-# encoder = hub.load('https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-128_A-2/2')
-# input = preprocess('this is a test.')
-# print(encoder(input))
-#
-# x = data['text']
-# y = data['score']
-#
-# trainx, testx, trainy, testy = train_test_split(x, y, test_size=0.2)
-# trainx, valx, trainy, valy = train_test_split(trainx, trainy, test_size=0.2)
-#
-# raw_trainx = tf.data.Dataset.from_tensor_slices(trainx)
-# raw_trainx = raw_trainx.batch(32, drop_remainder=True)
-#
-# raw_testx = tf.data.Dataset.from_tensor_slices(testx)
-# raw_testx = raw_testx.batch(32, drop_remainder=True)
-#
-# raw_valx = tf.data.Dataset.from_tensor_slices(valx)
-# raw_valx = raw_valx.batch(32)
-#
-# for i in raw_trainx:
-#     print(i)
-#
-# sns.set()
-# sns.displot(data=y)
-# plt.show()
-#
-#
-#
-# maxLen = 200
-# embSize = 64
-# vocabSize = 80000
-#
-# tk = Tokenizer(oov_token='oov_tok')
-# tk.fit_on_texts(x)
-# xSeq = tk.texts_to_sequences(x)
-# xPad = pad_sequences(xSeq, maxlen=maxLen, padding='post')
-# xTrain, xTest, yTrain, yTest = train_test_split(xPad, y, test_size=0.2)
-#
-#
-# model = Sequential()
-# model.add(layers.Embedding(200000, embSize, input_length=maxLen))
-# model.add(layers.SpatialDropout1D(0.5))
-# model.add(Bidirectional(layers.LSTM(embSize)))
-# model.add(layers.Dense(embSize, activation='relu'))
-# model.add(layers.Dense(3, activation='softmax'))
-#
-# model.summary()
-#
-# adam = tf.keras.optimizers.Adam(learning_rate=0.01, decay=0.1)
-#
-# model.compile(
-#     optimizer=adam,
-#     loss='sparse_categorical_crossentropy',
-#     metrics=['accuracy'],
-# )
-#
-# checkpointDir = os.path.join(ROOT_DIR, 'checkpoints', 'training.ckpt')
-#
-# cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpointDir,
-#                                                  save_weights_only=True,
-#                                                  monitor='val_accuracy',
-#                                                  verbose=1)
-#
-# log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-#
-# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir,
-#                                                       histogram_freq=1)
-#
-# reducer = tf.keras.callbacks.ReduceLROnPlateau(
-#     monitor='val_loss',
-#     factor=0.7,
-#     patience=2,
-#     verbose=1
-# )
-#
-# history = model.fit(
-#     xTrain,
-#     yTrain,
-#     epochs=30,
-#     batch_size=64,
-#     validation_split=0.2,
-#     callbacks=[cp_callback, tensorboard_callback, reducer],
-# )
